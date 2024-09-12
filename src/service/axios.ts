4635120861493c6ac9ca21f246170f2d615883f4
maycow jordny <maycowjordny2015@gmail.com>
2024-09-12 15:42:29 -0300

    adicionando baseUrl backend prod

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "https://api-calculator-calories-1.onrender.com",
 });
 
 export default axiosInstance;
