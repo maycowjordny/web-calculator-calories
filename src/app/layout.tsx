8882be15af211b2bc77f15f9c127c637cfb56275
maycow jordny <maycowjordny2015@gmail.com>
2024-09-16 09:56:35 -0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 config.autoAddCss = false;
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500"] });
@@ -21,11 +22,23 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2978516619638260"
           crossOrigin="anonymous"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V5X5LHR005"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V5X5LHR005');
+          `}
+        </Script>
       </head>
       <CalculateCaloriesThemeProvider>
         <body className={roboto.className}>
